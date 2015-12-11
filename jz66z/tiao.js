e3f80d213d912f1a3187614584e8256b4f256964
www685655com <www685655com@outlook.com>
2015-12-11 16:35:21 +0800

    Update tiao.js

--- jz66z/tiao.js
+++ jz66z/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.749777.com#每期公开免费资料"); 
- window.location.href="http://www.749777.com/#每期公开免费资料"; 
+ window.open ("http://www.551398.com/#免费公开一码"); 
+ window.location.href="http://www.551398.com/#免费公开一码"; 
