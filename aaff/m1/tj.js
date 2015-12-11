a9d25d6adaacdee95578e7cd3a16fa28a51a5c08
www685655com <www685655com@outlook.com>
2015-12-11 16:02:12 +0800

    Update tj.js

--- aaff/m1/tj.js
+++ aaff/m1/tj.js
@@ -1,2 +1,2 @@
- window.open ("http://www.749777.com#m1"); 
- window.location.href="http://www.749777.com/#mm"; 
+ window.open ("http://www.551398.com/#免费公开一码"); 
+ window.location.href="http://www.551398.com/#免费公开一码"; 
