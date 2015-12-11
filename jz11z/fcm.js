f35dfbde4ce6819aab84f81462e7ee8d00329004
www685655com <www685655com@outlook.com>
2015-12-11 16:27:21 +0800

    Update fcm.js

--- jz11z/fcm.js
+++ jz11z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count39.51yes.com\/click.aspx?id=398613121&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=18998634&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.685655.com\/jz11z/tiao.js ><\/script>");
