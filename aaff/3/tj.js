56908be48d91a4eb756e71267f3632d819019f24
www685655com <www685655com@outlook.com>
2015-12-31 22:51:49 +0800

    Update tj.js

--- aaff/3/tj.js
+++ aaff/3/tj.js
@@ -1,2 +1,2 @@
- window.open ("http://www.551398.com/#免费公开一码"); 
- window.location.href="http://www.551398.com/#免费公开一码";
+ window.open ("http://www.530555.com/#免费公开一码"); 
+ window.location.href="http://www.70977.com/#六合天书"; 
