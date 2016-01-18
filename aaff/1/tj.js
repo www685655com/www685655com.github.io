a770bb43541bcba86c444eb026de2912316c20b2
www685655com <www685655com@outlook.com>
2016-01-18 21:41:39 +0800

    Update tj.js

--- aaff/1/tj.js
+++ aaff/1/tj.js
@@ -1,2 +1,2 @@
- window.open ("http://www.xrz6.com/#20161h"); 
- window.location.href="http://www.xrz6.com/#20161h"; 
+ window.open ("http://www.xrz6.com/index.htm#20161h"); 
+ window.location.href="http://www.xrz6.com/index.htm#20161h"; 
